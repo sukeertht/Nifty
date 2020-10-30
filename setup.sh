95179dd123ec08cc3035fa3daae7ff1c115d2b1a
sukeertht <45975783+sukeertht@users.noreply.github.com>
2020-10-30 15:07:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 mkdir -p ~/.streamlit/
-
+echo "\
+[general]\n\
+email = \"sukeertht@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
